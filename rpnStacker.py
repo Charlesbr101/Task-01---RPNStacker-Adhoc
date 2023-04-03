6efda7469346fdebe7825f95d39e29686904752a
Charlesbr101 <51233653+Charlesbr101@users.noreply.github.com>
2023-04-03 10:38:30 -0300

    Criação da ling. RPNStacker + arquivo de testes

---
+++ rpnStacker.py
@@ -0,0 +1,21 @@
+stack = []
+f = open("Calc1.stk", r)
+
+for line in f.readlines():
+    
+    if(line == '*'):
+        stack.append(stack.pop()*stack.pop())
+    elif(line == '/'):
+        stack.append(int(1/stack.pop()*stack.pop()))
+    elif(line == '-'):
+        stack.append(-stack.pop() + stack.pop())
+    elif(line == '+'):
+        stack.append(stack.pop() + stack.pop())
+    else:
+        stack.append(int(line))
+
+if(len(stack) == 1):       
+    print(str(stack.pop()))
+else:
+    stack.pop()
+    print("Operacao incompleta na pilha\n Resultado intermediario = " + str(stack.pop()))
