277f9c52702309994ca6c5566a03d5244dd37af8
Charlesbr101 <51233653+Charlesbr101@users.noreply.github.com>
2023-04-03 13:49:47 +0000

    Fix linhas com newline

--- rpnStacker.py
+++ rpnStacker.py
@@ -1,8 +1,8 @@
 stack = []
-f = open("Calc1.stk", r)
+f = open("Calc1.stk", "r")
 
-for line in f.readlines():
-    
+for line in [l.rstrip() for l in f.readlines()]:
+
     if(line == '*'):
         stack.append(stack.pop()*stack.pop())
     elif(line == '/'):
